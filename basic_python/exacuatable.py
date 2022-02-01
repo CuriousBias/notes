8c3a89ecc28d93d14ecebd502c7e7aa3c5a5faa4
Kurt <kurt.partha@gmail.com>
2022-01-31 22:49:19 -0800

    added simple executable example

---
+++ basic_python/exacuatable.py
@@ -0,0 +1,24 @@
+import time
+
+def f1(text):
+    print(text)
+
+def f2(*numbers):
+    total = 0
+    for num in numbers:
+        total += num
+
+    return total
+
+
+# if __name__ == "__main__" checks to see if file __name__ (magic or dunder) is the __main__ 
+# Since it always is, proceeds to execute code below. 
+
+if __name__ == "__main__":
+
+    text = "hello world"
+    f1(text)
+
+    output = f2(1,2,3)
+
+    print(output)
